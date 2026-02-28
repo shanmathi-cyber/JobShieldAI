8a43692c29c4c53bf6b4cc80a0c84b1fd15df352
shanmathi-cyber <shancs164@rmkcet.ac.in>
2026-02-28 18:26:37 +0530

    Fixed Render PORT configuration

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request
 import re
 
@@ -148,4 +149,5 @@ def scan():
 # -----------------------------------
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
